796ec61ad0b139beb9fe3ef9bc8c6e43de742a34
vkh <vl@d.d>
2024-11-06 08:33:27 +0300

    Change Logout button

--- app/src/main/java/com/example/books/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/books/presentation/settings/SettingsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,7 +41,7 @@ fun SettingsScreen(
                     onLogOutClick(SignInObject)
                 }
             ) {
-
+                Text(text = "Log out")
             }
         }
     }
